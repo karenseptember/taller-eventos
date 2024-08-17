d8e1847afc020abd9134ff71b464eb799a1984c5
karenfigueroa <azucarbella7@gmail.com>
2024-08-17 00:41:43 -0300

    Motrar solo hola

--- eventos.js
+++ eventos.js
@@ -1,5 +1,13 @@
+let boton = document.querySelector(".botoncito");
 let div = document.querySelector("div");
 
-div.addEventListener("click",() =>{
-    alert("Hola! soy el div");
+
+boton.addEventListener("click",() =>{
+    alert("Hola!");
 })
+
+// Aplique el metodo remove al evento que esta saltando como error
+div.addEventListener.remove("click", () =>{
+    alert("Hola! Soy el div");
+})
+
